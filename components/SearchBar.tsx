4234f5656dd38b38c218e0773d10edb464ef1b0e
Angelhpascual <Angelhpascual@gmail.com>
2025-09-16 13:26:06 +0200

    feat: implement SearchBar component with local search parameters and input handling

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,29 @@
-import React from "react"
-import { Text, View } from "react-native"
+import { useLocalSearchParams } from "expo-router"
+import React, { useState } from "react"
+import { TextInput, TouchableOpacity, View } from "react-native"
 
 const SearchBar = () => {
+  const params = useLocalSearchParams<{ query?: string }>()
+
+  const [query, setQuery] = useState(params.query)
+
+  const handleSearch = (text: string) => {}
+
   return (
-    <View>
-      <Text>SearchBar</Text>
+    <View className="searchbar">
+      <TextInput
+        className="flex-1 p-5"
+        placeholder="Search for pizzas, burgers"
+        value={query}
+        onChangeText={handleSearch}
+        placeholderTextColor="#A0A0A0"
+      />
+      <TouchableOpacity
+        className="pr-5"
+        onPress={() => console.log("Search Pressed")}
+      >
+        JOSEPO DE LA BAHíA
+      </TouchableOpacity>
     </View>
   )
 }
